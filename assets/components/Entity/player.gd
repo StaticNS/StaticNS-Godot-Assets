1417c67f933622b94190997e6426bd06a00abf6a
StaticNS <98042782+StaticNS@users.noreply.github.com>
2024-02-02 12:04:42 -0500

    cleaned up player and velocity component

--- assets/components/Entity/player.gd
+++ assets/components/Entity/player.gd
@@ -1,65 +1,78 @@
 class_name Player
 extends CharacterBody2D
+## Player node for just having something to move around and such
 
-@export var velocityComponent: VelocityComponent
+@export var velocity_component: VelocityComponent
 
-var xCont = true
-var yCont = true
-var xMove: int = 0;
-var yMove: int = 0;
+var _x_cont := true
+var _y_cont := true
+var _x_move : int = 0;
+var _y_move : int = 0;
 
-#var inventory: Inventory
+#var inventory: Inventory = Inventory.new()
 
+
+## Not currently in use
 func _ready():
-	#inventory = GameHandler.initInventory("player_inventory", 2)
-	#inventory.PrintInventory()
 	pass
 
-func _process(delta):
-	var input = getInput()
-	if input: 
-		velocityComponent.AccelerateInDirection(input)
-	else:
-		velocityComponent.Decelerate()
-	velocityComponent.Move(self)
 
-func getInput():
+## Get input and do velocity_component component magic to move player around
+func _process(delta):
+	var input = _get_input()
+	if input: 
+		velocity_component.accelerate_in_direction(input)
+	else:
+		velocity_component.decelerate()
+	velocity_component.move(self)
+
+
+## For getting moving input only. 
+## Returns normalized Vector2 that the velocity component takes
+func _get_input():
 	if !Input.is_anything_pressed():
 		return
-		
+
 	var input = Vector2.ZERO
 
-	## This input system may seem overcomplicated but its here for a reason.
-	## In order to make the movement flow a smoothly as possibly for keyboard,
-	## we need to make sure that movement is dictated by our last input rather
-	## than two inputs on the same axis canceling each other out.
-	input.x = int(Input.is_action_pressed("ui_right")) + int(Input.is_action_pressed("ui_left"))
+	# This input system may seem overcomplicated but its here for a reason.
+	# In order to make the movement flow a smoothly as possibly for keyboard,
+	#we need to make sure that movement is dictated by our last input rather
+	#than two inputs on the same axis canceling each other out.
+
+	input.x = int(Input.is_action_pressed("ui_right"))\
+			 + int(Input.is_action_pressed("ui_left"))
 	match int(input.x):
 		1: 
-			xMove = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-			xCont = true
+			_x_move = int(Input.is_action_pressed("ui_right"))\
+					 - int(Input.is_action_pressed("ui_left"))
+			_x_cont = true
 		2:
-			if xCont:
-				xMove = int(Input.is_action_just_pressed("ui_right")) - int(Input.is_action_just_pressed("ui_left"))
-				xCont = false
+			if _x_cont:
+				_x_move = int(Input.is_action_just_pressed("ui_right"))\
+						 - int(Input.is_action_just_pressed("ui_left"))
+				_x_cont = false
 		_:
-			xMove = 0
-			xCont = true
+			_x_move = 0
+			_x_cont = true
 
-	input.y = int(Input.is_action_pressed("ui_down")) + int(Input.is_action_pressed("ui_up"))
+	input.y = int(Input.is_action_pressed("ui_down"))\
+			 + int(Input.is_action_pressed("ui_up"))
 	match int(input.y):
 		1: 
-			yMove = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-			yCont = true
+			_y_move = int(Input.is_action_pressed("ui_down")) \
+					- int(Input.is_action_pressed("ui_up"))
+			_y_cont = true
 		2:
-			if yCont:
-				yMove = int(Input.is_action_just_pressed("ui_down")) - int(Input.is_action_just_pressed("ui_up"))
-				yCont = false
+			if _y_cont:
+				_y_move = int(Input.is_action_just_pressed("ui_down")) \
+						- int(Input.is_action_just_pressed("ui_up"))
+				_y_cont = false
 		_:
-			yMove = 0
-			yCont = true
+			_y_move = 0
+			_y_cont = true
 
-	input.x = xMove
-	input.y = yMove
-	
+	input.x = _x_move
+	input.y = _y_move
+
 	return input.normalized()
--- assets/components/Node/velocity_component.gd
+++ assets/components/Node/velocity_component.gd
@@ -1,92 +1,134 @@
 class_name VelocityComponent
 extends Node
 
-@export var maxSpeed: float = 100:
+@export var max_speed : float = 100:
 	set(maxSpd):
-		maxSpeed = maxSpd
+		max_speed = maxSpd
 	get:
-		return maxSpeed * 10
-@export var accelerationCoefficient: float = 10:
+		return max_speed * 10
+@export var acceleration_coefficient : float = 10:
 	set(accelCoe):
-		accelerationCoefficient = accelCoe
+		acceleration_coefficient = accelCoe
 	get:
-		return accelerationCoefficient * 10
+		return acceleration_coefficient * 10
+@export var debug_mode := false
 
-@export var debugMode: bool = false
-
-var Velocity: Vector2 = Vector2.ZERO:
+var velocity := Vector2.ZERO:
 	set(vel):
-		Velocity = vel
+		velocity = vel
 	get:
-		return Velocity
-var SpeedMultiplier: float = 1.00:
+		return velocity
+var speed_multiplier : float = 1.00:
 	set(spdMulti):
-		SpeedMultiplier = spdMulti
+		speed_multiplier = spdMulti
 	get:
-		return SpeedMultiplier
-var SpeedPercentModifier = func(): 
+		return speed_multiplier
+var speed_percent_modifier := func() -> float: 
 	var sum: float = 0.00
-	for i in speedPercentModifiers.values():
+	for i in _speed_percent_modifiers.values():
 		sum += float(i)
 	return sum
-var AccelerationCoefficientMultiplier: float = 1.00:
-	set(accelCoeMulti):
-		AccelerationCoefficientMultiplier = accelCoeMulti
+var acceleration_coefficient_multiplier : float = 1.00:
+	set(accel_coe_multi):
+		acceleration_coefficient_multiplier = accel_coe_multi
 	get:
-		return AccelerationCoefficientMultiplier
+		return acceleration_coefficient_multiplier
 
-var SpeedPercent = func():
-	var maxSpeed = CalculatedMaxSpeed.call()
-	min(Velocity.length() / (maxSpeed if maxSpeed > 0.00 else 1.00), 1.00)
-var CalculatedMaxSpeed = func():
-	return maxSpeed * (1.00 + SpeedPercentModifier.call()) * SpeedMultiplier
+var _speed_percent = func():
+	var max_speed = _calculated_max_speed.call()
+	min(velocity.length() / (max_speed if max_speed > 0.00 else 1.00), 1.00)
+var _calculated_max_speed = func() -> float:
+	return max_speed * (1.00 + speed_percent_modifier.call()) * speed_multiplier
+var _speed_percent_modifiers : Dictionary = {}
 
-var speedPercentModifiers: Dictionary = {}
 
+## Ready function
 func _ready():
 	set_process(false)
-	if OS.is_debug_build() and debugMode:
-		(owner as Node2D).draw.connect(Callable(OnDebugDraw).bind(owner))
+	if OS.is_debug_build() and debug_mode:
+		(owner as Node2D).draw.connect(Callable(_on_debug_draw).bind(owner))
 		set_process(true)
 
+
+## Process function called every frame
 func _process(delta):
 	(owner as Node2D).queue_redraw()
 
-func AccelerateToVelocity(velocity: Vector2):
-	var accelTime = -accelerationCoefficient * AccelerationCoefficientMultiplier
-	var blend = -(1.00 - pow(1.00 - accelTime, get_process_delta_time() * 4.00))
-	Velocity = Velocity.lerp(velocity, blend) 
 
-func AccelerateInDirection(direction: Vector2):
-	AccelerateToVelocity(direction * CalculatedMaxSpeed.call())
+## Accelerates to given velocity with the components stats. 
+## DOES NOT MOVE PARENT[br]
+## [param velocity] velocity you want to accel to.
+func accelerate_to_velocity(velocity: Vector2):
+	var accel_time : float = -acceleration_coefficient\
+			 * acceleration_coefficient_multiplier
+	var blend : float = -(1.00 - pow(
+			1.00 - accel_time, 
+			get_process_delta_time() * 4.00
+	))
+	velocity = velocity.lerp(velocity, blend) 
 
-func GetMaxVelocity(direction: Vector2):
-	return direction * CalculatedMaxSpeed.call()
-	
-func MaximizeVelocity(direction: Vector2):
-	Velocity = GetMaxVelocity(direction)
 
-func Decelerate():
-	AccelerateToVelocity(Vector2.ZERO)
+## Similar to accelerate_to_velocity, but for directions instead. 
+## DOES NOT MOVE PARENT[br]
+## [param direction] direction you want to accel in.
+func accelerate_in_direction(direction: Vector2):
+	accelerate_to_velocity(direction * _calculated_max_speed.call())
 
-func Move(characterBody: CharacterBody2D):
-	characterBody.velocity = Velocity
-	characterBody.move_and_slide()
 
-func AddSpeedPercentModifier(name: String, change: float):
-	if speedPercentModifiers.has(name):
-		change += speedPercentModifiers[name]
-	speedPercentModifiers[name] = change
-	
-func SetSpeedPercentModifier(name: String, change: float):
-	speedPercentModifiers[name] = change
-	
-func GetSpeedPercentModifier(name: String):
-	if speedPercentModifiers.has(name):
-		return speedPercentModifiers[name]
+## Gets the calculated max velocity expected from moving in given direction.[br]
+## [param direction] direction to get max speed from.[br]
+## Returns a Vector2 of the direction multiplied by the max speed
+func get_max_velocity(direction: Vector2) -> Vector2:
+	return direction * _calculated_max_speed.call()
+
+
+## Immediately changes velocity to its maximum in a given direction.
+## DOES NOT MOVE PARENT[br]
+## [param direction] direction to maximize the velocity in.
+func maximize_velocity(direction: Vector2):
+	velocity = get_max_velocity(direction)
+
+
+## Decelerates the velocity until it reaches zero or is interupted. 
+## DOES NOT MOVE PARENT
+func decelerate():
+	accelerate_to_velocity(Vector2.ZERO)
+
+
+## Must be called if you want the velocity to be applied to the parent.[br]
+## [param character_body] CharacterBody2D to have velocity applied to.
+func move(character_body: CharacterBody2D):
+	character_body.velocity = velocity
+	character_body.move_and_slide()
+
+
+## Adds a percent modifier to the total max speed (stacks).[br]
+## [param name] name of the modifier for later access,[br]
+## [param change] the amount of change (in percent) wanted to apply to the maxspeed.
+func add_speed_percent_modifier(name: String, change: float):
+	if _speed_percent_modifiers.has(name):
+		change += _speed_percent_modifiers[name]
+	_speed_percent_modifiers[name] = change
+
+
+## Sets a percent modifier potetially replacing previous modifiers.[br]
+## [param name] name of the modifier to be set,[br]
+## [param change] the amount of change (in percent) wanted to apply to the maxspeed.
+func set_speed_percent_modifier(name: String, change: float):
+	_speed_percent_modifiers[name] = change
+
+
+## Gets a percent modifier value from its name.[br]
+## [param name] name of the modifier to be retrieved.[br]
+## Returns the value of the speed percent modifier
+func get_speed_percent_modifier(name: String) -> float:
+	if _speed_percent_modifiers.has(name):
+		return _speed_percent_modifiers[name]
 		
-	print("Dictionary \"speedPercentModifiers\" does not contain key: \"" + name + "\"")
+	print("Dictionary \"_speed_percent_modifiers\" does not contain key: \"" + name + "\"")
 	return 0.00
-	
-func OnDebugDraw(owner: Node2D):
-	owner.draw_line(Vector2.ZERO, Velocity, Color.CYAN, 2.00)
+
+
+## Debug draw call.
+func _on_debug_draw(owner: Node2D):
+	owner.draw_line(Vector2.ZERO, velocity, Color.CYAN, 2.00)
